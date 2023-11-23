e9125a3471e328f13a63b83f146887d6bf921acd
German Salinas <germansalinas.fce@gmail.com>
2023-11-23 20:33:36 -0300

    cambios

---
+++ BussinessLogic/DTO/UCTDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using DataAccess.Entities;
+
+namespace BussinessLogic.DTO
+{
+    public class UCTDTO
+    {
+        public int IdUct { get; set; }
+
+        public string? Codigo { get; set; }
+
+        public string? Denominacion { get; set; }
+
+        public string? Localidad { get; set; }
+
+        public string? Regional { get; set; }
+
+        public int? Tipo { get; set; }
+
+
+
+    }
+}
+
